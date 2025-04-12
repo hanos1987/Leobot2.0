b473cef912abaec4d52c2561f8067ebd395b718b
hanos1987 <benjfeir@protonmail.com>
2025-04-12 13:22:49 -0500

    Update utility_functions.py

--- utility/utility_functions.py
+++ utility/utility_functions.py
@@ -1,1 +1,12 @@
+import json
 
+def load_json(file):
+    try:
+        with open(file, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+def save_json(file, data):
+    with open(file, 'w') as f:
+        json.dump(data, f, indent=4)
